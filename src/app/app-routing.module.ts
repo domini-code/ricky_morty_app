f79c43854c3b2382b446ffbdaf5c8e94dde63b55
Bezael <bezael_e@hotmail.com>
2020-06-24 16:01:38 +0200

    Styles and route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,36 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./components/pages/home/home.module').then((m) => m.HomeModule),
+  },
 
-const routes: Routes = [];
+  {
+    path: 'character-list',
+    loadChildren: () =>
+      import(
+        './components/pages/characters/character-list/character-list.module'
+      ).then((m) => m.CharacterListModule),
+  },
+  {
+    path: 'character-details/:id',
+    loadChildren: () =>
+      import(
+        './components/pages/characters/character-details/character-details.module'
+      ).then((m) => m.CharacterDetailsModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
